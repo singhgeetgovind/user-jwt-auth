15364451fed0419b192e33efbdcfc97acfeb03fd
Geet2000 <singhgeet248@gmail.com>
2021-10-01 19:03:52 +0530

    minor initial comment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn usermanagerproj.wsgi --log-file -
--- usermanagerproj/settings.py
+++ usermanagerproj/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 from datetime import timedelta
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q*9tpm-9xnv8q8%uwn!dkbewwxnix-t1j3mg-ii#(@$@8!fg=%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['userjwtauth.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -163,9 +163,13 @@ AUTH_USER_MODEL ='userapp.CustomUser'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIR=[os.path.join(BASE_DIR, 'static'),]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
